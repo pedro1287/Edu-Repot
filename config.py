c5c914e6bb5774feef06d12ce0d346dae6be39ce
luisaes021 <119919226+luisaes021@users.noreply.github.com>
2022-12-17 18:55:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import ProxyCloud
 # Bot
-BOT_TOKEN = '5922890817:AAFCLOTU2acf9Pdd_4RVHCfuKSxKwkq2yL0'
+BOT_TOKEN = '5942144043:AAGm0uRg-Ia25MDXVBTbwP92yN4nNC-Qwng'
 TG_API_ID = '12168140'
 TG_API_HASH = '3504ce0eddb7dff4288d05d5e3dc5e4c'
 TG_ADMIN = 'Stvz20'
@@ -10,8 +10,8 @@ TG_ADMIN = 'Stvz20'
 # Database
 DB_LOCAL = False
 DB_HOST = 'db4free.net'
-DB_HOST_USERNAME = 'stvz02'
-DB_HOST_PASSWORD = 'Stvz123**'
+DB_HOST_USERNAME = ''
+DB_HOST_PASSWORD = ''
 DB_NAME = 'stvz02'
 
 if DB_LOCAL:
