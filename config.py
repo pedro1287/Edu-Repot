d0140e844134a79753d9f55ef7c7e0733316f7b0
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2022-12-17 22:21:50 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 import ProxyCloud
 # Bot
-BOT_TOKEN = '5942144043:AAGm0uRg-Ia25MDXVBTbwP92yN4nNC-Qwng'
+BOT_TOKEN = '5827823733:AAFCK6gdg-7et76hFZZZGQzm92YDDPvhUyQ'
 TG_API_ID = '12168140'
 TG_API_HASH = '3504ce0eddb7dff4288d05d5e3dc5e4c'
-TG_ADMIN = 'Stvz20'
+TG_ADMIN = 'JAGB2021'
 
 
 # Database
